f85ee468948cafad236ff6d9357f55cd73cfd007
wehart <wehart@sandia.gov>
2010-08-22 21:42:33 +0000

    Adding a dummy __init__.py file.  This ensures that the pyutilib.virtualenv preinstall works robustly.

---
+++ gcovr/__init__.py
@@ -0,0 +1,12 @@
+#  _________________________________________________________________________
+#
+#  FAST: Python tools for software testing.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the FAST README.txt file.
+#  _________________________________________________________________________
+
+# Empty gcovr package
+
