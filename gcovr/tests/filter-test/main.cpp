8578992ee7a143fe51c52b56600315a78eaf9983
jdsiiro <jdsiiro@sandia.gov>
2011-02-07 23:11:19 +0000

    - Improving estimation of the `<sources>` element when using gcovr with    filters.  - Adding a unit test for using gcovr with --filter.  - Adding revision and date keywords to gcovr so it is easier to    identify what version of the script users are using (especially when    they are running a snapshot from trunk).

---
+++ gcovr/tests/filter-test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int foo(int param) {
+  if (param) {
+     return 1; //std::cout << "param not null." << std::endl;
+  } else {
+     return 0; //std::cout << "param is null." << std::endl;
+  }
+}
+
+
+int main(int argc, char* argv[]) {
+  foo(0);
+
+  return 0;
+}
