623082c66431af614c70aca8a476dc28a039b968
wehart <wehart@sandia.gov>
2013-05-27 15:48:56 +0000

    Adding example2. Reformating the example1.cpp file.

--- doc/examples/example1/example1.cpp
+++ doc/examples/example1/example1.cpp
@@ -1,29 +1,40 @@
+// example1.cpp
+
 #include <iostream>
 
 #define MACRO()   if (1<0) foo(-1); else foo(1);
 
-int foo(int param) {
-  if (param) {
-     return 1; //std::cout << "param not null." << std::endl;
-  } else {
-     return 0; //std::cout << "param is null." << std::endl;
-  }
+int foo(int param)
+{
+    if (param)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-void bar(int param) {
-  if (param) {
-     std::cout << "param not null." << std::endl;
-  } else {
-     std::cout << "param is null." << std::endl;
-  }
+void bar(int param)
+{
+    if (param)
+    {
+        std::cout << "param not null." << std::endl;
+    }
+    else
+    {
+        std::cout << "param is null." << std::endl;
+    }
 }
 
 
-int main(int argc, char* argv[]) {
-  MACRO()
+int main(int argc, char* argv[])
+{
+    MACRO()
 
-  foo(0);
+    foo(0);
 
-  return 0;
+    return 0;
 }
 
---
+++ doc/examples/example2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. GetGlobals.sh
+export PATH=$CXXTEST/bin:$PATH:`pwd`/../../scripts
+
+cd example1
+ROOT=`pwd`
+
+# @compile:
+g++ -fprofile-arcs -ftest-coverage -fPIC -O0 example1.cpp -o program
+# @:compile
+
+# @run:
+./program
+# @:run
+
+# @gcovr:
+gcovr -r . -x
+# @:gcovr
+
+\rm -f program *.gc*
