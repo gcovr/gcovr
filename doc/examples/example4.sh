90c8c336e62d3d5306e7645330daa3f6a5fb8ed6
wehart <wehart@sandia.gov>
2013-07-04 20:38:13 +0000

    Adding HTML example

---
+++ doc/examples/example4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. GetGlobals.sh
+export PATH=$CXXTEST/bin:$PATH:`pwd`/../../scripts
+
+cd example1
+ROOT=`pwd`
+
+# @compile:
+g++ -fprofile-arcs -ftest-coverage -fPIC -O0 example1.cpp -o program
+# @:compile
+
+# @run:
+./program
+# @:run
+
+# @gcovr:
+gcovr -r . --html -o example1.html
+# @:gcovr
+mv example1.html ..
+
+\rm -f program *.gc*
