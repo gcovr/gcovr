b1009dd00f6eba7eea503b1e45b9300cd8d5bb62
mayeut <mayeut@users.noreply.github.com>
2018-01-09 22:06:24 +0100

    Fix most PEP 8 warnings + CI tests

--- doc/examples/test_examples.py
+++ doc/examples/test_examples.py
@@ -6,58 +6,62 @@ from os.path import dirname, abspath, basename
 import sys
 import re
 
-currdir = dirname(abspath(__file__))+os.sep
+currdir = dirname(abspath(__file__)) + os.sep
 datadir = currdir
 
 compilerre = re.compile("^(?P<path>[^:]+)(?P<rest>:.*)$")
-dirre      = re.compile("^([^%s]*/)*" % re.escape(os.sep))
-xmlre      = re.compile("\"(?P<path>[^\"]*/[^\"]*)\"")
-datere     = re.compile("date=\"[^\"]*\"")
-versionre  = re.compile("version=\"[^\"]*\"")
+dirre = re.compile("^([^%s]*/)*" % re.escape(os.sep))
+xmlre = re.compile("\"(?P<path>[^\"]*/[^\"]*)\"")
+datere = re.compile("date=\"[^\"]*\"")
+versionre = re.compile("version=\"[^\"]*\"")
 timestampre = re.compile("timestamp=\"[^\"]*\"")
-failure    = re.compile("^(?P<prefix>.+)file=\"(?P<path>[^\"]+)\"(?P<suffix>.*)$")
+failure = re.compile("^(?P<prefix>.+)file=\"(?P<path>[^\"]+)\"(?P<suffix>.*)$")
+
 
 def filter(line):
-    # for xml, remove prefixes from everything that looks like a 
+    # for xml, remove prefixes from everything that looks like a
     # file path inside ""
     line = xmlre.sub(
-            lambda match: '"'+re.sub("^[^/]+/", "", match.group(1))+'"',
-            line
-            )
+        lambda match: '"' + re.sub("^[^/]+/", "", match.group(1)) + '"',
+        line
+    )
     # Remove date info
-    line = datere.sub( lambda match: 'date=""', line)
+    line = datere.sub(lambda match: 'date=""', line)
     # Remove version info
-    line = versionre.sub( lambda match: 'version=""', line)
+    line = versionre.sub(lambda match: 'version=""', line)
     # Remove timestamp info
-    line = timestampre.sub( lambda match: 'timestamp=""', line)
+    line = timestampre.sub(lambda match: 'timestamp=""', line)
 
     if 'Running' in line:
         return False
     if "IGNORE" in line:
         return True
-    pathmatch = compilerre.match(line) # see if we can remove the basedir
-    failmatch = failure.match(line) # see if we can remove the basedir
-    #print "HERE", pathmatch, failmatch
+    pathmatch = compilerre.match(line)  # see if we can remove the basedir
+    failmatch = failure.match(line)  # see if we can remove the basedir
+    # print "HERE", pathmatch, failmatch
     if failmatch:
         parts = failmatch.groupdict()
-        #print "X", parts
+        # print "X", parts
         line = "%s file=\"%s\" %s" % (parts['prefix'], dirre.sub("", parts['path']), parts['suffix'])
     elif pathmatch:
         parts = pathmatch.groupdict()
-        #print "Y", parts
+        # print "Y", parts
         line = dirre.sub("", parts['path']) + parts['rest']
     return line
 
+
 # Declare an empty TestCase class
-class Test(unittest.TestCase): pass
+class Test(unittest.TestCase):
+    pass
+
 
 if not sys.platform.startswith('win'):
     # Find all *.sh files, and use them to define baseline tests
-    for file in glob.glob(datadir+'*.sh'):
+    for file in glob.glob(datadir + '*.sh'):
         bname = basename(file)
-        name=bname.split('.')[0]
-        if os.path.exists(datadir+name+'.txt'):
-            Test.add_baseline_test(cwd=datadir, cmd=file, baseline=datadir+name+'.txt', name=name, filter=filter)
+        name = bname.split('.')[0]
+        if os.path.exists(datadir + name + '.txt'):
+            Test.add_baseline_test(cwd=datadir, cmd=file, baseline=datadir + name + '.txt', name=name, filter=filter)
 
 # Execute the tests
 if __name__ == '__main__':
