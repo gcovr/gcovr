4f4ed299e54d672bbae2e7ce75260637642803aa
Lukas Atkinson <opensource@LukasAtkinson.de>
2018-02-06 18:52:51 +0100

    docs: update include_anchors script for Python 3

--- doc/include_anchors.py
+++ doc/include_anchors.py
@@ -1,81 +1,56 @@
+from __future__ import print_function
 import re
 import sys
 import os.path
 import os
 
 
-pat1a = re.compile('include::([a-zA-Z0-9_\.\-/\/]+\/)\.([^\_]+)\_[a-zA-Z0-9]*\.py\[\]')
-pat1b = re.compile('include::([a-zA-Z0-9_\.\-/\/]+\/)\.([^\_]+)\_[a-zA-Z0-9]*\.sh\[\]')
-pat1c = re.compile('include::([a-zA-Z0-9_\.\-/\/]+\/)\.([^\_]+)\_[a-zA-Z0-9]*\.h\[\]')
-pat1d = re.compile('include::([a-zA-Z0-9_\.\-/\/]+\/)\.([^\_]+)\_[a-zA-Z0-9]*\.cpp\[\]')
-pat2 = re.compile('([^@]+)@([a-zA-Z0-9]+):')
-pat3 = re.compile('([^@]+)@:([a-zA-Z0-9]+)')
-
-processed = set()
+include_pattern = re.compile(r'include::([a-zA-Z0-9_.\-/]+\/)\.([^_]+)\_[a-zA-Z0-9]*\.(py|sh|h|cpp)\[\]')
+anchor_start_pattern = re.compile('([^@]+)@([a-zA-Z0-9]+):')
+anchor_end_pattern = re.compile('([^@]+)@:([a-zA-Z0-9]+)')
 
 
 def process(dir, root, suffix):
-    # print "PROCESS ",root, suffix
-    bname = "%s%s" % (dir, root)
-    global processed
-    if bname in processed:
-        return
-    #
     anchors = {}
     anchors[''] = open('%s.%s_.%s' % (dir, root, suffix), 'w')
-    INPUT = open('%s%s.%s' % (dir, root, suffix), 'r')
-    for line in INPUT:
-        m2 = pat2.match(line)
-        m3 = pat3.match(line)
-        if m2:
-            anchor = m2.group(2)
-            anchors[anchor] = open('%s.%s_%s.%s' % (dir, root, anchor, suffix), 'w')
-        elif m3:
-            anchor = m3.group(2)
-            anchors[anchor].close()
-            del anchors[anchor]
-        else:
+    with open('%s%s.%s' % (dir, root, suffix), 'r') as INPUT:
+        for line in INPUT:
+            m = anchor_start_pattern.match(line)
+            if m:
+                anchor = m.group(2)
+                anchor_file = '%s.%s_%s.%s' % (dir, root, anchor, suffix)
+                anchors[anchor] = open(anchor_file, 'w')
+                continue
+            m = anchor_end_pattern.match(line)
+            if m:
+                anchor = m.group(2)
+                anchors[anchor].close()
+                del anchors[anchor]
+                continue
             for anchor in anchors:
-                os.write(anchors[anchor].fileno(), line)
-    INPUT.close()
+                print(line, file=anchors[anchor])
     for anchor in anchors:
         if anchor != '':
             print("ERROR: anchor '%s' did not terminate" % anchor)
         anchors[anchor].close()
-    #
-    processed.add(bname)
 
 
+processed = set()
 for file in sys.argv[1:]:
     print("Processing file '%s' ..." % file)
-    INPUT = open(file, 'r')
-    for line in INPUT:
-        suffix = None
-        m = pat1a.match(line)
-        if m:
-            suffix = 'py'
-        #
-        if suffix is None:
-            m = pat1b.match(line)
-            if m:
-                suffix = 'sh'
-        #
-        if suffix is None:
-            m = pat1c.match(line)
-            if m:
-                suffix = 'h'
-        #
-        if suffix is None:
-            m = pat1d.match(line)
-            if m:
-                suffix = 'cpp'
-        #
-        if suffix is not None:
-            # print "HERE", line, suffix
-            fname = m.group(1) + m.group(2) + '.' + suffix
-            if not os.path.exists(fname):
+    with open(file, 'r') as INPUT:
+        for line in INPUT:
+            m = include_pattern.match(line)
+            if not m:
+                continue
+            directory, root, suffix = m.groups()
+            filename = directory + root + '.' + suffix
+            basename = directory + root
+            if not os.path.exists(filename):
                 print(line)
-                print("ERROR: file '%s' does not exist!" % fname)
+                print("ERROR: file '%s' does not exist!" % filename)
                 sys.exit(1)
-            process(m.group(1), m.group(2), suffix)
-    INPUT.close()
+            if basename in processed:
+                continue
+            process(directory, root, suffix)
+            processed.add(basename)
