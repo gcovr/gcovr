f2439146ce8fe34082adf7566cf821c4d5255f46
Jeremy Fixemer <jeremyfixemer@gmail.com>
2019-05-05 10:37:37 -0500

    fh.write causing error, print w/ file spec

--- gcovr/cobertura_xml_generator.py
+++ gcovr/cobertura_xml_generator.py
@@ -217,11 +217,12 @@ def print_xml_report(covdata, options):
 
     with smart_open(options.output) as fh:
         if LXML_AVAILABLE:
-            fh.write(
+            print(
                 etree.tostring(root,
                                pretty_print=options.prettyxml,
                                encoding="UTF-8",
                                xml_declaration=True,
-                               doctype="<!DOCTYPE coverage SYSTEM 'http://cobertura.sourceforge.net/xml/coverage-04.dtd'>"))
+                               doctype="<!DOCTYPE coverage SYSTEM 'http://cobertura.sourceforge.net/xml/coverage-04.dtd'>"),
+                file=fh)
         else:
-            fh.write(etree.tostring(root, encoding="UTF-8"))
+            print(etree.tostring(root, encoding="UTF-8"), file=fh)
