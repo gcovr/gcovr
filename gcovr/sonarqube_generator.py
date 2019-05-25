5048b8108f451cdd61b94f9fa0a61bbb93732fcc
Lukas Atkinson <opensource@LukasAtkinson.de>
2019-05-25 15:01:20 +0200

    port sonarqube generator to lxml

--- gcovr/sonarqube_generator.py
+++ gcovr/sonarqube_generator.py
@@ -2,28 +2,21 @@
 
 # This file is part of gcovr <http://gcovr.com/>.
 #
-# Copyright 2013-2018 the gcovr authors
+# Copyright 2013-2019 the gcovr authors
 # Copyright 2013 Sandia Corporation
 # This software is distributed under the BSD license.
 
 import os
-import xml.dom.minidom
+from lxml import etree
 
-try:
-    xrange
-except NameError:
-    xrange = range
+from .utils import open_binary_for_writing
 
 
-#
-# Produce an XML report in the Sonarqube generic coverage format
-#
 def print_sonarqube_report(covdata, options):
+    """produce an XML report in the Sonarqube generic coverage format"""
 
-    impl = xml.dom.minidom.getDOMImplementation()
-    doc = impl.createDocument(None, "coverage", None)
-    root = doc.documentElement
-    root.setAttribute("version", "1")
+    root = etree.Element("coverage")
+    root.set("version", "1")
     source_dirs = set()
 
     for f in sorted(covdata):
@@ -42,35 +35,35 @@ def print_sonarqube_report(covdata, options):
             directory = f
         directory, fname = os.path.split(directory)
 
-        fileNode = doc.createElement("file")
+        fileNode = etree.Element("file")
 
         filename = os.path.join(directory, fname).replace('\\', '/')
-        fileNode.setAttribute("path", filename)
+        fileNode.set("path", filename)
 
         for lineno in sorted(data.lines):
             line_cov = data.lines[lineno]
             if not line_cov.is_covered and not line_cov.is_uncovered:
                 continue
 
-            L = doc.createElement("lineToCover")
-            L.setAttribute("lineNumber", str(lineno))
+            L = etree.Element("lineToCover")
+            L.set("lineNumber", str(lineno))
             if line_cov.is_covered:
-                L.setAttribute("covered", "true")
+                L.set("covered", "true")
             else:
-                L.setAttribute("covered", "false")
+                L.set("covered", "false")
 
             branches = line_cov.branches
             if branches:
                 b_total, b_hits, coverage = line_cov.branch_coverage()
-                L.setAttribute("branchesToCover", str(b_total))
-                L.setAttribute("coveredBranches", str(b_hits))
+                L.set("branchesToCover", str(b_total))
+                L.set("coveredBranches", str(b_hits))
 
-            fileNode.appendChild(L)
+            fileNode.append(L)
 
-        root.appendChild(fileNode)
+        root.append(fileNode)
 
-    xmlString = doc.toprettyxml(indent="")
-
-    OUTPUT = open(options.sonarqube, 'w')
-    OUTPUT.write(xmlString + '\n')
-    OUTPUT.close()
+    with open_binary_for_writing(options.sonarqube) as fh:
+        fh.write(
+            etree.tostring(root,
+                           encoding="UTF-8",
+                           xml_declaration=True))
