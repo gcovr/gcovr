8319157a351c4c46aff6634bffad79cf376b1060
Spacetown <40258682+Spacetown@users.noreply.github.com>
2020-07-20 10:36:34 +0200

    Added review points from chrta.

--- gcovr/utils.py
+++ gcovr/utils.py
@@ -181,12 +181,10 @@ FilterOption.NonEmpty = NonEmptyFilterOption
 class Filter(object):
     def __init__(self, pattern):
         cwd = os.getcwd()
-        # Gueesing if file system is case insensitive.
-        # The working directory is not the root and accessable in upper and lower case.
+        # Guessing if file system is case insensitive.
+        # The working directory is not the root and accessible in upper and lower case.
         is_fs_case_insensitive = ( cwd != os.path.sep ) and os.path.exists(cwd.upper()) and os.path.exists(cwd.lower())
-        flags = 0
-        if is_fs_case_insensitive:
-            flags |= re.IGNORECASE
+        flags = re.IGNORECASE if is_fs_case_insensitive else 0
         self.pattern = re.compile(pattern, flags)
 
     def match(self, path):
