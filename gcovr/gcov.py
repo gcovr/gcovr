838f377e92ec2a082563fe80c54d5298bab35f14
Spacetown <michael.foerderer@gmx.de>
2020-09-02 21:32:49 +0200

    Change comment and remove redundant code.

--- gcovr/gcov.py
+++ gcovr/gcov.py
@@ -205,14 +205,15 @@ def guess_source_file_name_heuristics(
     if os.path.exists(fname):
         return fname
 
-    # 3. Try using the source directory as the source directory
-    fdir = os.path.dirname(source_fname)
-    fname = os.path.join(fdir, gcovname)
+    # Get path of gcda file
+    source_fname_dir = os.path.dirname(source_fname)
+
+    # 3. Try using the path to the gcda as the source directory
+    fname = os.path.join(source_fname_dir, gcovname)
     if os.path.exists(fname):
         return os.path.normpath(fname)
 
-    # 4. Try using the path to the gcda file as the source directory
-    source_fname_dir = os.path.dirname(source_fname)
+    # 4. Try using the path to the gcda file as the source directory, removing the path part from the gcov file
     fname = os.path.join(source_fname_dir, os.path.basename(gcovname))
     return fname
 
