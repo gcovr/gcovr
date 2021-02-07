2aef4345cf7628124b23ef7f0844380b98dab572
Michael Förderer <40258682+Spacetown@users.noreply.github.com>
2021-02-07 09:36:57 +0100

    Change JSON format version to "0.2" (#463)
    * Update format version and add explanation to assert.

--- gcovr/tests/test_args.py
+++ gcovr/tests/test_args.py
@@ -358,7 +358,7 @@ def test_html_injection_via_json(capsys, tmp_path):
 
     script = '<script>alert("pwned")</script>'
     jsondata = {
-        'gcovr/format_version': "0.1",
+        'gcovr/format_version': "0.2",
         'files': [
             {'file': script, 'lines': []},
             {'file': 'other', 'lines': []},
