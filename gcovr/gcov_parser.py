6dec6b0646452a8343621a4a6dc9f94acbf5cab2
Spacetown <michael.foerderer@gmx.de>
2022-04-01 22:06:45 +0200

    Update copyright text.

--- gcovr/gcov_parser.py
+++ gcovr/gcov_parser.py
@@ -9,9 +9,10 @@
 #
 # Copyright (c) 2013-2022 the gcovr authors
 # Copyright (c) 2013 Sandia Corporation.
-# This software is distributed under the BSD License.
 # Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 # the U.S. Government retains certain rights in this software.
+#
+# This software is distributed under the 3-clause BSD License.
 # For more information, see the README.rst file.
 #
 # ****************************************************************************
