gcovr/gcovr

commit b186a6f04f8b62a8f88c6fb0f60af36af778fe34

Add color themes for html report #381
- Adapt HTML for same look in Firefox and Safari.

gcovr/templates/style.css
@@ -1,280 +1,19 @@
-:root { font-family: sans-serif; }
+:root {
+    font-family: sans-serif;
+    --unknown_color: {{unknown_color}};
+    --low_color: {{low_color}};
+    --medium_color: {{medium_color}};
+    --high_color: {{high_color}};
+    --covered_color: {{covered_color}};
+    --uncovered_color: {{uncovered_color}};
+    --takenBranch_color: {{takenBranch_color}};
+    --notTakenBranch_color: {{notTakenBranch_color}};
+}
 
 body
 {
     color: #000000;
     background-color: #FFFFFF;
-}
-
-/* Link formats: use maroon w/underlines */
-a:link
-{
-    color: navy;
-    text-decoration: underline;
-}
-a:visited
-{
-    color: maroon;
-    text-decoration: underline;
-}
-
-/*** TD formats ***/
-
-/* TD Header Information */
-th[scope=row]
-{
-    text-align: right;
-    color: black;
-    padding-right: 6px;
-    font-weight: bold;
-    vertical-align: top;
-    white-space: nowrap;
-}
-th[scope=col],
-table.legend td
-{
-    text-align: left;
-    color: blue;
-    font-weight: bold;
-    white-space: nowrap;
-}
-
-.coverage td
-{
-    text-align: right;
-    color: black;
-    font-weight: bold;
-    white-space: nowrap;
-    padding-left: 12px;
-    padding-right: 4px;
-    background-color: LightBlue;
-}
-
-table.legend td.legend
-{
-    color: black;
-    font-size: 80%;
-    font-weight: normal;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: flex-start;
-}
-
-td.legend > span
-{
-    margin: 2px;
-}
-
-td.coverage-unknown,
-span.coverage-unknown
-{
-    background-color: LightGray;
-}
-
-td.coverage-none,
-span.coverage-none,
-td.coverage-low,
-span.coverage-low
-{
-    background-color: {{low_color}} !important;
-}
-
-td.coverage-medium,
-span.coverage-medium
-{
-    background-color: {{medium_color}} !important;
-}
-
-td.coverage-high,
-span.coverage-high
-{
-    background-color: {{high_color}} !important;
-}
-
-/* Coverage Table */
-
-.file-list td, .file-list th {
-    padding: 0 10px;
-    font-weight: bold;
-}
-
-.file-list th[scope^=col]
-{
-    text-align: center;
-    color: white;
-    background-color: SteelBlue;
-    font-size: 120%;
-}
-
-.file-list th[scope=row]
-{
-    text-align: left;
-    color: black;
-    font-family: monospace;
-    font-weight: bold;
-    font-size: 110%;
-}
-
-.file-list tr > td,
-.file-list tr > th {
-    background: LightBlue;
-}
-
-.file-list tr:nth-child(even) > td,
-.file-list tr:nth-child(even) > th {
-	  background: LightSteelBlue
-}
-
-td.CoverValue
-{
-    text-align: right;
-    white-space: nowrap;
-}
-
-meter {
-    -moz-appearance: none;
-
-    width: 30vw;
-    min-width: 4em;
-    max-width: 15em;
-    height: 12px;
-    padding: 0;
-    background: white;
-    vertical-align: baseline;
-}
-
-meter::-webkit-meter-bar {
-    background: none;
-    background-color: white;
-}
-
-meter::-webkit-meter-optimum-value,
-meter::-webkit-meter-suboptimum-value,
-meter::-webkit-meter-even-less-good-value
-{
-    border: solid 1px black;
-}
-
-meter::-moz-meter-bar
-{
-    box-sizing: border-box;
-}
-
-meter:-moz-meter-optimum::-moz-meter-bar,
-meter:-moz-meter-sub-optimum::-moz-meter-bar,
-meter:-moz-meter-sub-sub-optimum::-moz-meter-bar
-{
-    border: solid 1px black;
-}
-
-meter.coverage-none:-moz-meter-optimum::-moz-meter-bar,
-meter.coverage-low:-moz-meter-optimum::-moz-meter-bar
-{
-    background: red;
-}
-
-meter.coverage-medium:-moz-meter-optimum::-moz-meter-bar
-{
-    background: yellow;
-}
-
-meter.coverage-high:-moz-meter-optimum::-moz-meter-bar
-{
-    background: green;
-}
-
-td.coveredLine,
-span.coveredLine
-{
-    background-color: {{covered_color}}!important;
-}
-
-td.uncoveredLine,
-span.uncoveredLine
-{
-    background-color: {{uncovered_color}}!important;
-}
-
-.linebranch, .linecount
-{
-    font-family: monospace;
-    border-right: 1px gray solid;
-    background-color: lightgray;
-    text-align: right;
-}
-
-.linebranchDetails
-{
-    position: relative;
-}
-.linebranchSummary
-{
-    cursor: help;
-}
-.linebranchContents
-{
-    font-family: sans-serif;
-    font-size: small;
-    text-align: left;
-    position: absolute;
-    width: 15em;
-    padding: 1em;
-    background: white;
-    border: solid gray 1px;
-    box-shadow: 5px 5px 10px gray;
-}
-
-.takenBranch
-{
-    color: {{takenBranch_color}}!important;
-}
-
-.notTakenBranch
-{
-    color: {{notTakenBranch_color}}!important;
-}
-
-.src
-{
-    padding-left: 12px;
-    text-align: left;
-
-    font-family: monospace;
-    white-space: pre;
-
-    tab-size: {{tab_size}};
-    -moz-tab-size: {{tab_size}};
-}
-
-.srcHeader,
-span.takenBranch,
-span.notTakenBranch
-{
-    font-family: monospace;
-    font-weight: bold;
-}
-
-pre
-{
-    height : 15px;
-    margin-top: 0;
-    margin-bottom: 0;
-}
-
-.source-line
-{
-    height : 15px;
-    margin-top: 0;
-    margin-bottom: 0;
-}
-.lineno
-{
-    background-color: #EFE383;
-    border-right: 1px solid #BBB15F;
-    text-align: right;
-    unicode-bidi: embed;
-    font-family: monospace;
-    white-space: pre;
 }
 
 h1
@@ -289,53 +28,29 @@ h1
 hr
 {
     background-color: navy;
-    height: 3px;
+    height: 2px;
     border: 0;
 }
 
-.summary
+/* Link formats: use maroon w/underlines */
+a:link
 {
-    display: flex;
-    flex-flow: row wrap;
-    max-width: 100%;
-    justify-content: flex-start;
+    color: navy;
+    text-decoration: underline;
 }
-
-.summary > table
+a:visited
 {
-    flex: 1 0 7em;
-    border: 0;
+    color: maroon;
+    text-decoration: underline;
 }
 
-.summary > :last-child {
-    margin-left: auto;
-}
+{% include "style.summary.css" %}
 
-.coverage .headerValue
-{
-    text-align: right;
-}
+{% include "style.meter.css" %}
 
-.file-list
-{
-    margin: 1em auto;
-    border: 0;
-    border-spacing: 1px;
-}
+{% include "style.filelist.css" %}
 
-.file-source table
-{
-    border-spacing: 0;
-}
-
-.file-source table td {
-    padding: 1px 10px;
-}
-
-.file-source table td:last-child
-{
-    width: 100%;
-}
+{% include "style.details.css" %}
 
 footer
 {

gcovr/templates/style.details.css
@@ -1,0 +1,128 @@
+td.CoverValue
+{
+    text-align: right;
+    white-space: nowrap;
+}
+
+td.coveredLine,
+span.coveredLine
+{
+    background-color: var(--covered_color) !important;
+}
+
+td.uncoveredLine,
+span.uncoveredLine
+{
+    background-color: var(--uncovered_color) !important;
+}
+
+.linebranch, .linecount
+{
+    font-family: monospace;
+    border-right: 1px gray solid;
+    background-color: lightgray;
+    text-align: right;
+}
+
+.linebranchDetails
+{
+    position: relative;
+}
+.linebranchSummary
+{
+    cursor: help;
+}
+.linebranchContents
+{
+    font-family: sans-serif;
+    font-size: small;
+    text-align: left;
+    position: absolute;
+    width: 15em;
+    padding: 1em;
+    background: white;
+    border: solid gray 1px;
+    box-shadow: 5px 5px 10px gray;
+    z-index: 1; /* show in front of the table entries */
+}
+
+.takenBranch
+{
+    color: var(--takenBranch_color) !important;
+}
+
+.notTakenBranch
+{
+    color: var(--notTakenBranch_color) !important;
+}
+
+.src
+{
+    padding-left: 12px;
+    text-align: left;
+
+    font-family: monospace;
+    white-space: pre;
+
+    tab-size: var(--tab_size);
+    -moz-tab-size: var(--tab_size);
+}
+
+span.takenBranch,
+span.notTakenBranch
+{
+    font-family: monospace;
+    font-weight: bold;
+}
+
+pre
+{
+    height : 15px;
+    margin-top: 0;
+    margin-bottom: 0;
+}
+
+.source-line
+{
+    height : 15px;
+    margin-top: 0;
+    margin-bottom: 0;
+}
+.lineno
+{
+    background-color: #EFE383;
+    border-right: 1px solid #BBB15F;
+    text-align: right;
+    unicode-bidi: embed;
+    font-family: monospace;
+    white-space: pre;
+}
+
+.file-list
+{
+    margin: 1em auto;
+    border: 0;
+    border-spacing: 1px;
+}
+
+.file-source table
+{
+    border-spacing: 0;
+}
+
+.file-source table td,
+.file-source table th
+{
+    padding: 1px 10px;
+}
+
+.file-source table th
+{
+    font-family: monospace;
+    font-weight: bold;
+}
+
+.file-source table td:last-child
+{
+    width: 100%;
+}

commit 2eeff05afe30e7b212f8fdaefeca77a51aa87950

handle HTML themes in CSS only; restore tab-size feature
Previously, colors for the themes were specified in Python code.

Now, the colors for all themes are listed in the CSS styles. The current theme
is selected via a "theme-green" or "theme-blue" class on the "<html>" element.

In the future, this would make it possible to switch themes at runtime with a
bit of JavaScript that changes the classes in the DOM.

While making this change, it was discovered that the "tab_size" variable wasn't
used properly. This fixes the regression in
b186a6f04f8b62a8f88c6fb0f60af36af778fe34 / #381.

gcovr/templates/base.html
@@ -1,6 +1,6 @@
 {# -*- engine: jinja -*- #}
 <!DOCTYPE html>
-<html>
+<html class="theme-{{theme}}">
 
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset={{info.encoding}}"/>

gcovr/templates/style.css
@@ -1,19 +1,28 @@
 :root {
     font-family: sans-serif;
-    --unknown_color: {{unknown_color}};
-    --low_color: {{low_color}};
-    --medium_color: {{medium_color}};
-    --high_color: {{high_color}};
-    --covered_color: {{covered_color}};
-    --uncovered_color: {{uncovered_color}};
-    --excluded_color: {{excluded_color}};
-    --warning_color: {{warning_color}};
-    --takenBranch_color: {{takenBranch_color}};
-    --notTakenBranch_color: {{notTakenBranch_color}};
-    --takenDecision_color: {{takenDecision_color}};
-    --uncheckedDecision_color: {{uncheckedDecision_color}};
-    --notTakenDecision_color: {{notTakenDecision_color}}
+    --tab_size: {{tab_size}};
+}
 
+.theme-green, .theme-blue {
+    --unknown_color: lightgray;
+    --low_color: #FF6666;
+    --medium_color: #F9FD63;
+    --high_color: #85E485;
+    --covered_color: #85E485;
+    --uncovered_color: #FF8C8C;
+    --excluded_color: #53BFFD;
+    --warning_color: orangered;
+    --takenBranch_color: green;
+    --notTakenBranch_color: red;
+    --takenDecision_color: green;
+    --uncheckedDecision_color: darkorange;
+    --notTakenDecision_color: red;
+}
+
+.theme-blue {
+    --high_color: #66B4FF;
+    --covered_color: #66B4Ff;
+    --takenBranch_color: blue;
 }
 
 body

gcovr/templates/style.details.css
@@ -64,17 +64,17 @@ span.excludedLine
 
 .takenDecision
 {
-    color: {{takenDecision_color}}!important;
+    color: var(--takenDecision_color) !important;
 }
 
 .notTakenDecision
 {
-    color: {{notTakenDecision_color}}!important;
+    color: var(--notTakenDecision_color) !important;
 }
 
 .uncheckedDecision
 {
-    color: {{uncheckedDecision_color}}!important;
+    color: var(--uncheckedDecision_color) !important;
 }
 
 .src

gcovr/writer/html.py
@@ -102,56 +102,29 @@ def user_templates():
 
 class CssRenderer:
 
-    Themes = {
-        "green": {
-            "unknown_color": "LightGray",
-            "low_color": "#FF6666",
-            "medium_color": "#F9FD63",
-            "high_color": "#85E485",
-            "covered_color": "#85E485",
-            "uncovered_color": "#FF8C8C",
-            "excluded_color": "#53BFFD",
-            "warning_color": "orangered",
-            "takenBranch_color": "Green",
-            "notTakenBranch_color": "Red",
-            "takenDecision_color": "Green",
-            "uncheckedDecision_color": "DarkOrange",
-            "notTakenDecision_color": "Red",
-        },
-        "blue": {
-            "unknown_color": "LightGray",
-            "low_color": "#FF6666",
-            "medium_color": "#F9FD63",
-            "high_color": "#66B4FF",
-            "covered_color": "#66B4FF",
-            "uncovered_color": "#FF8C8C",
-            "excluded_color": "#53BFFD",
-            "warning_color": "orangered",
-            "takenBranch_color": "Blue",
-            "notTakenBranch_color": "Red",
-            "takenDecision_color": "Green",
-            "uncheckedDecision_color": "DarkOrange",
-            "notTakenDecision_color": "Red",
-        },
-    }
+    THEMES = ["green", "blue"]
 
     @staticmethod
     def get_themes():
-        return list(CssRenderer.Themes.keys())
+        return CssRenderer.THEMES
 
     @staticmethod
     def get_default_theme():
-        return "green"
+        return CssRenderer.THEMES[0]
+
+    @staticmethod
+    def load_css_template(options):
+        if options.html_css is not None:
+            template_path = os.path.relpath(options.html_css)
+            return user_templates().get_template(template_path)
+
+        return templates().get_template("style.css")
 
     @staticmethod
     def render(options):
-        template = None
-        if options.html_css is not None:
-            template = user_templates().get_template(os.path.relpath(options.html_css))
-        else:
-            template = templates().get_template("style.css")
+        template = CssRenderer.load_css_template(options)
         return template.render(
-            CssRenderer.Themes[options.html_theme], tab_size=options.html_tab_size
+            tab_size=options.html_tab_size,
         )
 
 
@@ -379,6 +352,8 @@ def print_html_report(covdata: CovData, output_file, options):
         else:
             css_link = css_output
         data["css_link"] = css_link
+
+    data["theme"] = options.html_theme
 
     root_info.set_coverage(covdata)
 