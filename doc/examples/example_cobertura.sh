54424445eaef0eb56cf93ac636532947a079f9a8
Spacetown <michael.foerderer@gmx.de>
2022-09-09 21:25:37 +0200

    Add validation of cobertura report with libxml.

--- doc/examples/example_cobertura.sh
+++ doc/examples/example_cobertura.sh
@@ -8,4 +8,8 @@ ${CXX:-g++} -fprofile-arcs -ftest-coverage -fPIC -O0 example.cpp -o program
 gcovr --cobertura-pretty
 #END gcovr
 
+if [[ "$OSTYPE" != "msys" ]]; then
+xmllint --valid --noout example_cobertura.xml || exit 1
+fi
+
 rm -f program *.gc*
