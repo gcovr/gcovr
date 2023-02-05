084b3b5fe8c33d21e4e056f7e44a863819951804
whart222 <whart222@gmail.com>
2023-02-05 06:29:15 -0700

    Convert directories with realpath

--- gcovr/utils.py
+++ gcovr/utils.py
@@ -259,8 +259,8 @@ class DirectoryPrefixFilter(Filter):
         super().__init__(pattern)
 
     def match(self, path: str):
-        realpath = os.path.normpath(path)
-        return super().match(realpath)
+        path = os.path.normpath(realpath(path))
+        return super().match(path)
 
 
 def configure_logging() -> None:
