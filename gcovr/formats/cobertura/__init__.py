a289dc4df90b23b33f2b0f5c64fecf6df4894e1b
Josef Barnes <joe@barnesmail.net>
2023-06-30 22:01:14 +1000

    Add support for importing Cobertura coverage file

--- gcovr/formats/cobertura/__init__.py
+++ gcovr/formats/cobertura/__init__.py
@@ -28,6 +28,9 @@ from ...coverage import CovData
 class CoberturaHandler(BaseHandler):
     def get_options() -> List[GcovrConfigOption]:
         return [
+            # Global options used for merging.
+            "merge_mode_functions",
+            # Local options
             GcovrConfigOption(
                 "cobertura",
                 ["--cobertura", "-x", "--xml"],
@@ -52,7 +55,24 @@ class CoberturaHandler(BaseHandler):
                 ),
                 action="store_true",
             ),
+            GcovrConfigOption(
+                "cobertura_add_tracefile",
+                ["--cobertura-add-tracefile"],
+                config="cobertura-add-tracefile",
+                help=(
+                    "Combine the coverage data from Cobertura XML files. "
+                    "When this option is used gcov is not run to collect "
+                    "the new coverage data."
+                ),
+                action="append",
+                default=[],
+            ),
         ]
+
+    def read_report(self) -> CovData:
+        from .read import read_report
+
+        return read_report(self.options)
 
     def write_report(self, covdata: CovData, output_file: str) -> None:
         from .write import write_report
