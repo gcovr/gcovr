66cad6700953959080098ad3e35240ae1569eac9
Michael Förderer <michael.foerderer@gmx.de>
2023-09-06 21:57:32 +0200

    Use relative paths for gcov if possible (#820)
    If path arguments to gcov can be represented as relative paths to the
    current working directory of gcov use the relative paths.

--- gcovr/formats/gcov/read.py
+++ gcovr/formats/gcov/read.py
@@ -635,12 +635,24 @@ def run_gcov_and_process_files(
         # This lock is essential for parallel processing because without
         # this there can be name collisions for the generated output files.
         with locked_directory(chdir):
+            filename = abs_filename
+            # Use try catch because the relpath can fail on Windows for different drives.
+            # Do not know how to force this exception therefore ignore coverage.
+            try:
+                filename = os.path.relpath(filename, chdir)
+            except Exception:  # pragma: no cover
+                pass
+            object_directory = os.path.dirname(abs_filename)
+            try:
+                object_directory = os.path.relpath(object_directory, chdir)
+            except Exception:  # pragma: no cover
+                pass
             out, err = gcov_cmd.run_with_args(
                 [
                     abs_filename,
                     *gcov_cmd.get_default_options(),
                     "--object-directory",
-                    os.path.dirname(abs_filename),
+                    object_directory,
                 ],
                 cwd=chdir,
             )
