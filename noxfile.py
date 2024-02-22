b21dad63d6a226ed94c57a8f2d7656598b132458
Michael Förderer <40258682+Spacetown@users.noreply.github.com>
2024-02-22 07:03:01 +0100

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -313,7 +313,7 @@ def tests_compiler(session: nox.Session, version: str) -> None:
         *args,
         success_codes=[0, 1] if running_locally and coverage_args else [0],
     )
-    
+
     if os.environ.get("USE_COVERAGE") == "true":
         session.run("coverage", "xml")
         if running_locally:
