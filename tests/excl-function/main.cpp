626252c912f220d0a8c6f86c2d3dcd8c51cddf27
Spacetown <40258682+spacetown@users.noreply.github.com>
2024-09-04 22:42:24 +0200

    Extend test with function definition after marker

--- tests/excl-function/main.cpp
+++ tests/excl-function/main.cpp
@@ -28,24 +28,29 @@ int /* GCOVR_EXCL_FUNCTION */ foo(int param) { // GCOVR_EXCL_FUNCTION
 }
 
 #if defined USE_LAMBDA
-void sort_excluded(void) // GCOVR_EXCL_FUNCTION
-{
-   std::array<int, 10> arr = { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+#define LAMBDA_SORT \
+   { \
+      std::array<int, 10> arr = { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 }; \
+      std::sort( \
+         std::begin(arr), \
+         std::end(arr), \
+         [](int a, int b) { \
+            if (a > b) \
+               return true; \
+            return false; \
+         } \
+      ); \
+   }
 
-   std::sort(
-      std::begin(arr),
-      std::end(arr),
-      [](int a, int b) {
-         if (a > b)
-            return true;
-
-         return false;
-      }
-   );
+// Function in line with exclude
+void sort_excluded(void) /* GCOVR_EXCL_FUNCTION */ { LAMBDA_SORT /* THIS is not excluded*/
+   LAMBDA_SORT
 }
 
 void sort_lambda_excluded(void)
 {
+   LAMBDA_SORT // GCOVR_EXCL_FUNCTION not working because after function definition
+
    std::array<int, 10> arr = { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 };
 
    std::sort(
