9f5bae5505b69b1d39e8d87dadf5d5d165538848
Spacetown <40258682+spacetown@users.noreply.github.com>
2024-09-20 17:46:20 +0200

    Print stderr of gcov in verbose mode

--- gcovr/formats/gcov/read.py
+++ gcovr/formats/gcov/read.py
@@ -812,7 +812,9 @@ class GcovProgram:
         """
         gcov_process = self.__get_gcov_process(args, **kwargs)
         out, err = gcov_process.communicate()
-        LOGGER.debug(f"GCOV return code was {gcov_process.returncode}")
+        LOGGER.debug(
+            f"GCOV return code was {gcov_process.returncode}, stderr was:\n{err}<<"
+        )
         if gcov_process.returncode < 0:
             raise RuntimeError(
                 f"GCOV returncode was {gcov_process.returncode} (exited by signal).\n"
