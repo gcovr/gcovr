21e6cf4c8f7a7d7f8006a662aaaef656cdbe3fd8
Spacetown <40258682+spacetown@users.noreply.github.com>
2025-07-14 18:15:07 +0200

    Improve devcontainer
    - Install extensions in container.
    - Install dependencies on container startup.
    - Configure IDE to use installed tools and configurations.
    - Format files on save.

--- noxfile.py
+++ noxfile.py
@@ -332,7 +332,7 @@ def mypy(session: nox.Session) -> None:
     if session.posargs:
         args = session.posargs
     else:
-        args = ["--install-types", "--non-interactive", "."]
+        args = ["."]
     session.run("mypy", *args)
 
 
