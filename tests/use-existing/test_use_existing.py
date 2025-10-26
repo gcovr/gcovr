60bec926ae732f16b2a076019cdc14add631c2df
Spacetown <40258682+spacetown@users.noreply.github.com>
2025-10-26 20:20:41 +0100

    Add test for filtering existing files

--- tests/use-existing/test_use_existing.py
+++ tests/use-existing/test_use_existing.py
@@ -4,9 +4,8 @@ if typing.TYPE_CHECKING:
     from tests.conftest import GcovrTestExec
 
 
-def test(gcovr_test_exec: "GcovrTestExec") -> None:
-    """A test that verifies coverage when using existing *.gcov coverage files with verbose output.
-    All files identical to tests/simple1 except for this README file and Makefile"""
+def test_all(gcovr_test_exec: "GcovrTestExec") -> None:
+    """A test that verifies coverage when using existing *.gcov coverage files."""
     gcovr_test_exec.cxx_link(
         "testcase",
         "main.cpp",
@@ -21,6 +20,31 @@ def test(gcovr_test_exec: "GcovrTestExec") -> None:
     )
     gcovr_test_exec.gcovr(
         "--gcov-use-existing-files",
+        "--gcov-delete",
+        "--json-pretty",
+        "--json=coverage.json",
+    )
+    gcovr_test_exec.compare_json()
+
+
+def test_exclude_existing(gcovr_test_exec: "GcovrTestExec", check) -> None:  # type: ignore[no-untyped-def]
+    """A test that verifies exclusion when using existing *.gcov coverage files."""
+    gcovr_test_exec.cxx_link(
+        "testcase",
+        "main.cpp",
+    )
+    gcovr_test_exec.run("./testcase")
+    gcovr_test_exec.run(
+        *gcovr_test_exec.gcov(),
+        *list(gcovr_test_exec.output_dir.glob("*.gcda")),
+        "--branch-counts",
+        "--branch-probabilities",
+        "--preserve-paths",
+    )
+    gcovr_test_exec.gcovr(
+        "--verbose",
+        "--gcov-use-existing-files",
+        "--gcov-exclude=.*main.*",
         "--gcov-delete",
         "--json-pretty",
         "--json=coverage.json",
