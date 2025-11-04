caa1c5f05f8d85be7a42b09e74198f18e99fd047
Michael Förderer <40258682+Spacetown@users.noreply.github.com>
2025-11-04 18:00:52 +0100

    Apply suggestions from code review
    Co-authored-by: David Legault <dlegaultbbry@users.noreply.github.com>

--- src/gcovr/formats/llvm/__init__.py
+++ src/gcovr/formats/llvm/__init__.py
@@ -2,7 +2,7 @@
 
 #  ************************** Copyrights and license ***************************
 #
-# This file is part of gcovr 8.3+main, a parsing and reporting tool for gcov.
+# This file is part of gcovr 8.4+main, a parsing and reporting tool for gcov.
 # https://gcovr.com/en/main
 #
 # _____________________________________________________________________________
@@ -29,7 +29,7 @@ LOGGER = logging.getLogger("gcovr")
 
 
 class LlvmHandler(BaseHandler):
-    """Class to handle own JSON tracefile format."""
+    """Class to handle LLVM JSON tracefile format."""
 
     @classmethod
     def get_options(cls) -> list[Union[GcovrConfigOption, str]]:
--- src/gcovr/formats/llvm/read.py
+++ src/gcovr/formats/llvm/read.py
@@ -2,7 +2,7 @@
 
 #  ************************** Copyrights and license ***************************
 #
-# This file is part of gcovr 8.3+main, a parsing and reporting tool for gcov.
+# This file is part of gcovr 8.4+main, a parsing and reporting tool for gcov.
 # https://gcovr.com/en/main
 #
 # _____________________________________________________________________________
