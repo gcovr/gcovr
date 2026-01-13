532df02bf8d73731ca03e037b76a74d797fd7153
Spacetown <40258682+spacetown@users.noreply.github.com>
2026-01-13 20:59:56 +0100

    Prepare next iteration

--- tests/filter/test_filter.py
+++ tests/filter/test_filter.py
@@ -2,8 +2,8 @@
 
 #  ************************** Copyrights and license ***************************
 #
-# This file is part of gcovr 8.6, a parsing and reporting tool for gcov.
-# https://gcovr.com/en/8.6
+# This file is part of gcovr 8.6+main, a parsing and reporting tool for gcov.
+# https://gcovr.com/en/main
 #
 # _____________________________________________________________________________
 #
